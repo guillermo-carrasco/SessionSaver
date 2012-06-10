ee3ca3c4de8f88b7ba26dafa20a89fb037c4d74a
Guillermo Carrasco Hernandez <guillem@guillem-desktop.(none)>
2012-06-10 23:11:38 +0200

    Finished version 1.0

--- SessionSaver.py
+++ SessionSaver.py
@@ -23,7 +23,10 @@ class SaveAsCommand(sublime_plugin.WindowCommand):
 		sublime.status_message("Session saved succefully as " + sessionName + '!')
 
 
-class LoadSession(sublime_plugin.WindowCommand):
+class LoadSessionCommand(sublime_plugin.WindowCommand):
+	"""
+	Close all currently opened tabs and open the document(s) oh the selected session
+	"""
 	def run(self):
 		sessionsDir = os.path.join(sublime.packages_path(), 'SessionSaver/sessions')
 		if not os.listdir(sessionsDir):
@@ -34,12 +37,27 @@ class LoadSession(sublime_plugin.WindowCommand):
 	def on_done(self, session):
 		session = sublime.packages_path() + '/SessionSaver/sessions/' + os.listdir(sublime.packages_path() + '/SessionSaver/sessions/')[session]
 		#Close current views
-		#self.window.run_command("close_all")
+		self.window.run_command("close_all")
 		#Open saved documents
 		s = open(session, 'r')
 		for l in s.readlines():
-			self.window.open_file(l)
+			self.window.open_file(l.rstrip())
 		s.close()
-			
 
-## General TODOs: Remove sessions (asking if sure...), check things... 
+
+class RemoveSessionCommand(sublime_plugin.WindowCommand):
+	"""
+	Removes a stored session WITHOUT confirmation
+	"""
+	def run(self):
+		sessionsDir = os.path.join(sublime.packages_path(), 'SessionSaver/sessions')
+		if not os.listdir(sessionsDir):
+			sublime.status_message("There are no sessions saved")
+		else:
+			self.window.show_quick_panel(os.listdir(sessionsDir), self.on_done)
+
+	def on_done(self, session):
+		session = sublime.packages_path() + '/SessionSaver/sessions/' + os.listdir(sublime.packages_path() + '/SessionSaver/sessions/')[session]
+		#Remove the session file
+		os.remove(session)
+		sublime.status_message("Session succefully removed")
